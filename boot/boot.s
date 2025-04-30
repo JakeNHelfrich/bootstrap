518ea8f41d17f93ff68a9d18bb71d46fa64734ce
Jake Helfrich <jakenichelfrich@gmail.com>
2025-05-01 00:20:47 +0800

    quit after carriage return entered

--- boot/boot.s
+++ boot/boot.s
@@ -5,24 +5,26 @@ _start:
     lui   t1, 0x10000         # a0 = 0x10000005 (UART line status register)
     addi  t1, t1, 0x005       #
 
+    addi  t2, x0, 0x00D       # carriage return
+
 loop:
-    jal   ra, get_user_input
+    jal   ra, get_char
     jal   ra, print_char
+    
+    beq   a1, t2, end
+
     j     loop
+
+get_char:
+    lb    a1, 0(t1)
+    andi  a2, a1, 1
+    beqz  a2, get_char
+    
+    lb    a1, 0(t0)
+    jalr  x0, 0(ra)
 
 print_char:
     sb    a1, 0(t0)
-    j     go_back
-
-get_user_input:
-    lb    a1, 0(t1)
-    andi  a2, a1, 1
-    beqz  a2, get_user_input
-    
-    lb    a1, 0(t0)
-    j     go_back
-
-go_back:
-    jalr  x0, 0(ra)
+    jalr  x0, 0(ra) 
 
 end:
